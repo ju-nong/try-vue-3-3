85b7871f2d75d454ec4f485038c105df28b4b039
이준용 <iiiuooyoo@gmail.com>
2024-04-24 09:36:37 +0900

    backup

---
+++ src/components/pages/PropsComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{ name }}, {{ msg }}</div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const { name = "홍길동", msg = "어서오고" } = defineProps<{
+    name?: string;
+    msg?: string;
+}>();
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/pages/index.ts
@@ -0,0 +1,3 @@
+import PropsComponents from "./PropsComponent.vue";
+
+export { PropsComponents };
